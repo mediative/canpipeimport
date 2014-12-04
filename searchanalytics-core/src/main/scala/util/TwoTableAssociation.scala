e32548425b6e8d3d1c12305ec72dfc7e69e75779
Luis Da Costa <dacosta.le@gmail.com>
2014-12-04 06:18:04 -0500

    Tables Associations are type-parametrized (to allow for wider associations on the foreign key).

--- searchanalytics-core/src/main/scala/util/TwoTableAssociation.scala
+++ searchanalytics-core/src/main/scala/util/TwoTableAssociation.scala
@@ -3,8 +3,8 @@ package util
 /**
  * Represents an association between 2 tables when schema is normalized
  */
-trait TwoTableAssociation {
-  def fk1: Long
-  def fk2: Long
+trait TwoTableAssociation[T1, T2] {
+  def fk1: T1
+  def fk2: T2
 }
 
