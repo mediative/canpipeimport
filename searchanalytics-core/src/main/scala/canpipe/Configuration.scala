38da9d61ae3030ad9679d83ea742a94caa8b2eef
Luis Da Costa <dacosta.le@gmail.com>
2014-12-04 06:02:56 -0500

    Add 'Configuration' class + creation in a package object

---
+++ searchanalytics-core/src/main/scala/canpipe/Configuration.scala
@@ -0,0 +1,27 @@
+package canpipe
+
+/**
+ * Encapsulates the 2 types of directories we will work with destination files:
+ * a working directory, temporal
+ * the actual output directory
+ */
+case class DstFolders(workingTmp: String, output: String)
+
+// TODO: read configuration fron a file.
+class Configuration {
+
+  /**
+   * root hdfs directory where data will live, once generated
+   */
+  private def locationForDstFiles = "/source/canpipe/parquet"
+
+  /**
+   * A working directory where we can dump files to be processed
+   */
+  private def workingTmp = s"${locationForDstFiles}/workingTmp"
+
+  val eventsFolders = DstFolders(workingTmp = s"${workingTmp}/events", output = s"${locationForDstFiles}/events")
+  val headingsFolders = DstFolders(workingTmp = s"${workingTmp}/headings", output = s"${locationForDstFiles}/headingsXevents")
+  val directoriesFolders = DstFolders(workingTmp = s"${workingTmp}/directories", output = s"${locationForDstFiles}/directoriesXevents")
+
+}
