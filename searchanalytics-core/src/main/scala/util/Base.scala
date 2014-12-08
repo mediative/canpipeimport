e8e39c7b43a9fa28fd942c42ca709e98c0916219
Luis Da Costa <dacosta.le@gmail.com>
2014-12-08 11:15:52 -0500

    Util: ability of parse Strings as booleans + filling up a list

--- searchanalytics-core/src/main/scala/util/Base.scala
+++ searchanalytics-core/src/main/scala/util/Base.scala
@@ -3,7 +3,8 @@ package util
 // import com.typesafe.scalalogging.Logging
 // import com.typesafe.scalalogging.slf4j.StrictLogging
 
-object Base { // extends Logging {
+object Base {
+  // extends Logging {
 
   def getCurrentTimeStamp: java.sql.Timestamp = {
     new java.sql.Timestamp(java.util.Calendar.getInstance().getTime.getTime)
@@ -29,12 +30,36 @@ object Base { // extends Logging {
    * http://www.amazon.com/Beginning-Scala-David-Pollak/dp/1430219890
    */
   def using[A <: { def close(): Unit }, B](param: A)(f: A => B): B =
-    try { f(param) } finally { param.close() }
+    try {
+      f(param)
+    } finally {
+      param.close()
+    }
 
   object String {
 
     def generateRandom(length: Int): String = scala.util.Random.alphanumeric.take(length).mkString("")
+    def toBooleanOpt(s: String): Option[Boolean] = {
+      s.trim.toUpperCase match {
+        case "TRUE" | "1" | "T" | "TRUE" | "Y" | "YES" => Some(true)
+        case "FALSE" | "0" | "F" | "FALSE" | "N" | "NO" => Some(false)
+        case x if (x.isEmpty) => Some(false)
+        case _ => None
+      }
+    }
 
   }
 
+  /**
+   * Ensures a list meets minimal size, filling it up, if needed, with a defautl value.
+   * @param aList Reference (starting) list
+   * @param n Size to be met
+   * @param default Values the list will be filled with if not enough elements are present in initial list
+   * @return a list with at least 'n' elements of type T.
+   */
+  def fillToMinimumSize[T](aList: List[T], n: Int, default: T): List[T] = {
+    aList ++ List.tabulate(n - aList.length)(_ => default)
+  }
+
 }
+
--- searchanalytics-core/src/test/scala/util/BaseTest.scala
+++ searchanalytics-core/src/test/scala/util/BaseTest.scala
@@ -1,6 +1,7 @@
 package util
 
 import org.scalatest.{ BeforeAndAfter, FlatSpec }
+import util.Base._
 
 class BaseTest extends FlatSpec with BeforeAndAfter {
 
@@ -23,4 +24,38 @@ class BaseTest extends FlatSpec with BeforeAndAfter {
     assert(listOf10RandomStrings.forall { anElem => listOf10RandomStrings.filter(_ == anElem).length == 1 })
   }
 
+  val stringsThatAreBooleanAndTrue = List("true", "t", "T")
+  val stringsThatAreBooleanAndFalse = List("false", "False", "NO", "f", "F")
+
+  "Converting a String to Boolean" should s"return 'true' when String is one of ${stringsThatAreBooleanAndTrue.mkString(start = "{", end = "}", sep = ",")} " in {
+    stringsThatAreBooleanAndTrue foreach { aString =>
+      val bOpt = String.toBooleanOpt(aString)
+      assert(bOpt.isDefined)
+      assert(bOpt.get)
+    }
+  }
+
+  it should s"return 'false' when String is one of ${stringsThatAreBooleanAndFalse.mkString(start = "{", end = "}", sep = ",")} " in {
+    stringsThatAreBooleanAndFalse foreach { aString =>
+      val bOpt = String.toBooleanOpt(aString)
+      assert(bOpt.isDefined)
+      assert(!bOpt.get)
+    }
+  }
+
+  "Filling a list up to a certain size" should "work when list is already full" in {
+    val l = List(1, 2, 3)
+    assert(fillToMinimumSize[Int](aList = l, n = 1, default = -1) == l)
+  }
+
+  it should "work when size is negative" in {
+    val l = List(1, 2, 3)
+    assert(fillToMinimumSize[Int](aList = l, n = -1, default = -1) == l)
+  }
+
+  it should "work in a general setting" in {
+    val l = List(1, 2, 3)
+    val theSize = 5
+    assert(fillToMinimumSize[Int](aList = l, n = theSize, default = -1).length == theSize)
+  }
 } // end of file
