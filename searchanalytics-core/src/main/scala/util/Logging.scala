b7a0409f42e64e64b6be298056eff7de2bb9565a
Luis Da Costa <dacosta.le@gmail.com>
2015-02-10 14:15:52 -0500

    Logging: add specific loggers for each level of verbosity

--- searchanalytics-core/src/main/scala/util/Logging.scala
+++ searchanalytics-core/src/main/scala/util/Logging.scala
@@ -7,10 +7,38 @@ package util
 /**
  * Wrap-up against any logger we'd like.
  */
+
+object Logging extends Serializable {
+  object Level extends Enumeration {
+    type Level = Value
+    val Silent, Info, Debug, Error = Value
+  }
+}
+
 trait Logging extends Serializable {
+  import Logging.Level._
+
+  def level: Level = Logging.Level.Info
+
   object logger extends Serializable {
-    def info(msg: String) = println(s"[INFO] ${msg}")
-    def debug(msg: String) = println(s"[DEBUG] ${msg}")
+    def info(msg: String) = if (level == Info) println(s"[INFO] ${msg}")
+    def debug(msg: String) = if ((level == Info) || (level == Debug)) println(s"[DEBUG] ${msg}")
     def error(msg: String) = println(s"[ERROR] ${msg}")
   }
+}
+
+trait InfoLogging extends Logging {
+  override val level = Logging.Level.Info
+}
+
+trait SilentLogging extends Logging {
+  override val level = Logging.Level.Silent
+}
+
+trait DebugLogging extends Logging {
+  override val level = Logging.Level.Debug
+}
+
+trait ErrorLogging extends Logging {
+  override val level = Logging.Level.Error
 }
